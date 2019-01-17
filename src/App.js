40d05ff4beca4b8b06ae487543fcbd0021b65a2c
Omer Omer <omeror@g.cofc.edu>
2019-01-17 14:01:58 -0500

    Add new Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import RandomMeme from './Components/RandomMeme';
 import CatPic from './Components/CatPics'
 import NavBar from './Components/NavBar'
 import './App.css'
+import TvShowInfo from './Components/TvShowInfo';
+
 class App extends Component {
  
     render() { 
@@ -11,11 +13,14 @@ class App extends Component {
             <div className="wrapper" >
                 <NavBar/>       <br/>
                 <div className="row">
-                    <p className="col-md-2"/>
+                   
                     <RandomMeme className="col-md-4"/>
-                    <p className="col-md-2"/>
+                   
                     <CatPic className="col-md-4"/>
-                </div>    
+                    <TvShowInfo className="col-md-4"/>
+                </div> 
+                <br/>
+                
             </div>
       );
     }
